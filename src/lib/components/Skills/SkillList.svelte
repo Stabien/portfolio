fe00aebdf57e13962ca3afbea8a75267ddd8c039
Stabien <bastien.piedallu@gmail.com>
2024-09-03 22:29:51 +0200

    refactor(skills): use each operator

--- src/lib/components/Skills/SkillList.svelte
+++ src/lib/components/Skills/SkillList.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import SkillListItem from './SkillListItem.svelte';
 	import skills from '$lib/assets/data/skills.json';
 	import type { Skill } from '../../../types';
 	import SkillListByTag from './SkillListByTag.svelte';
@@ -15,16 +14,43 @@
 	};
 
 	const skillByTag = groupByTag(skills);
+
+	const tags = [
+		{
+			key: 'front',
+			title: 'Front-end'
+		},
+		{
+			key: 'fullstack',
+			title: 'Full stack'
+		},
+		{
+			key: 'back',
+			title: 'Back-end'
+		},
+		{
+			key: 'database',
+			title: 'Bases de données'
+		},
+		{
+			key: 'devops',
+			title: 'DevOps'
+		},
+		{
+			key: 'testing',
+			title: 'Testing'
+		},
+		{
+			key: 'mobile',
+			title: 'Mobile'
+		}
+	];
 </script>
 
 <div class="flex flex-row gap-4">
 	<div class="flex flex-col gap-8 mx-auto">
-		<SkillListByTag title="Front-end" skills={skillByTag.front} />
-		<SkillListByTag title="Back-end" skills={skillByTag.back} />
-		<SkillListByTag title="Full stack" skills={skillByTag.fullstack} />
-		<SkillListByTag title="Base de données" skills={skillByTag.database} />
-		<SkillListByTag title="DevOps" skills={skillByTag.devops} />
-		<SkillListByTag title="Testing" skills={skillByTag.testing} />
-		<SkillListByTag title="Mobile" skills={skillByTag.mobile} />
+		{#each tags as tag}
+			<SkillListByTag title={tag.title} skills={skillByTag[tag.key]} />
+		{/each}
 	</div>
 </div>
